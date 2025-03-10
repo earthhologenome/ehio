228b67a7ecaa4ed14052c8aac5ce02d908565276
EisenRa <raph.eisenhofer@gmail.com>
2025-03-10 13:05:07 +0100

    fix path

--- cli.py
+++ cli.py
@@ -10,7 +10,7 @@ from pathlib import Path
 from ehio.utils import *
 
 ## setup paths (hardcoded)
-EHIO_PATH = "/projects/ehi/data/0_Environments/EHIO/ehio"
+EHIO_PATH = "/projects/ehi/data/0_Environments/ehio"
 EHI_SOFTWARE_PATH = "/projects/ehi/data/0_Code/EHI_bioinformatics_1.1"
 
 
